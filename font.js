17feec7cce0ff4d95268fa3a11d04991b55719e6
Geury Roustand <theromantic2010@hotmail.com>
2021-07-01 16:48:09 +0200

    In process

--- font.js
+++ font.js
@@ -1,38 +1,53 @@
 const key =
   "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGRjNWZkZWIzNTgxNzAwMTVjMjI3MDkiLCJpYXQiOjE2MjUwNTUxOTksImV4cCI6MTYyNjI2NDc5OX0.7iWVJHAsRtVZQp-fg2i5UsDxH-lGvBdX3abGB789JG8";
+const url = "https://striveschool-api.herokuapp.com/api/product/";
 
-// <a href="#" data-id='${product._id}' class="btn btn-primary">Delate </a>
 const row = document.querySelector(".row");
-
-const card = document.getElementsByClassName("delate ");
-
-console.log(card);
+const bgImg = document.querySelector(".bg-img");
 
 const displayProducts = (products) => {
-  products.forEach((product) => {
-    // <img src="${product.imageUrl}" class="card-img-top" alt="...">
-    // console.log(product);
-    // row.innerHTML = "";
+  //   const [product] = products;
 
-    // const div = document.createElement("div");
+  //   const html = `
+
+  //   <div class="col delate mt-5 data-id='${product._id}'">
+  //   <div class="card" style="width: 18rem;">
+  //     <div class="card-body">
+  //       <h5 class="card-title"> ${product.name}</h5>
+  //       <p class="card-text">${product.description}</p>
+  //       <p class="card-text">${product.price} €</p>
+  //       <p class="card-text">${product.brand}</p>
+  //       <a href="./detail.html?productId=${product._id} " class="btn btn-primary">Buy It Now </a>
+
+  //     </div>
+  //   </div>
+  // </div>
+
+  //   `;
+  //   row.insertAdjacentHTML("afterbegin", html);
+
+  products.forEach((product) => {
+    // const img = document.createElement("img");
+    // img.src = product.imageUrl;
+    // const image = (bgImg.style.backgroundImage = `src${product.imageUrl}`);
 
     const html = `
-    
-  <div class="col delate mt-5 data-id='${product._id}'">
-  <div class="card" style="width: 18rem;">
-  <img src="${product.imageUrl}" class="card-img-top " alt="...">
-    <div class="card-body">
-      <h5 class="card-title"> ${product.name}</h5>
-      <p class="card-text">${product.description}</p>
-      <p class="card-text">${product.price} €</p>
-      <p class="card-text">${product.brand}</p>
-      <a href="#" class="btn btn-primary">Buy It Now </a>
-  
-    </div>
-  </div>
-</div>
-
-  `;
+      
+        <div class="col delate mt-5 data-id='${product._id}'">
+        <div class="card" style="width: 18rem;">
+        <img src="${product.imageUrl}" class="card-img-top " alt="...">
+          <div class="card-body">
+            <h5 class="card-title"> ${product.name}</h5>
+            <p class="card-text">${product.description}</p>
+            <p class="card-text">${product.price} €</p>
+            <p class="card-text">${product.brand}</p>
+            <a href="./detail.html?productId=${product._id} " class="btn btn-primary">Buy It Now </a>
+        
+          </div>
+        </div>
+      </div>
+      
+        `;
 
     row.insertAdjacentHTML("afterbegin", html);
   });
@@ -59,14 +74,11 @@ const displayProducts = (products) => {
 // Fetching Using await
 
 const getDataWithAwait = async () => {
-  const response = await fetch(
-    "https://striveschool-api.herokuapp.com/api/product/",
-    {
-      headers: {
-        Authorization: key,
-      },
-    }
-  );
+  const response = await fetch(url, {
+    headers: {
+      Authorization: key,
+    },
+  });
 
   const data = await response.json();
   console.log(data);
@@ -78,10 +90,6 @@ getDataWithAwait();
 window.onload = () => {
   getDataWithAwait();
 };
-
-card.forEach((e) => {
-  console.log(e);
-});
 
 // const delateCard = (e) => {
 //   console.log(e.curen);
